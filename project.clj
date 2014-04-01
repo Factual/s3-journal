72041893dc51cd96423a4c2cd5427a059c3250ed
Zach Tellman <ztellman@gmail.com>
2014-04-01 12:22:33 -0700

    update to latest durable-queue

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :dependencies [[com.amazonaws/aws-java-sdk "1.6.10" :exclusions [commons-codec]]
-                 [factual/durable-queue "0.1.1-SNAPSHOT"]
+  :dependencies [[com.amazonaws/aws-java-sdk "1.7.5" :exclusions [commons-codec]]
+                 [factual/durable-queue "0.1.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [byte-transforms "0.1.2"]]
   :jvm-opts ^:replace ["-server" "-Xmx8g"]
