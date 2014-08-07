5768f4f0cef99dd4665f3c61a96e075f2f66942d
Zach Tellman <ztellman@gmail.com>
2014-08-07 14:07:47 -0700

    mark 0.1.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject factual/s3-journal "0.1.0-SNAPSHOT"
+(defproject factual/s3-journal "0.1.0"
   :description "Reliable, high-throughput journalling to S3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :dependencies [[com.amazonaws/aws-java-sdk "1.7.6" :exclusions [commons-codec]]
-                 [factual/durable-queue "0.1.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [byte-transforms "0.1.2"]]
+  :dependencies [[com.amazonaws/aws-java-sdk "1.8.7" :exclusions [commons-codec]]
+                 [factual/durable-queue "0.1.2"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [byte-transforms "0.1.3"]]
   :jvm-opts ^:replace ["-server" "-Xmx8g"]
   :global-vars {*warn-on-reflection* true})
